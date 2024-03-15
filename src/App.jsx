3d160701f28c68db36fd96e039a1deb4ddd75df3
arifulislamk <ariful18374@gmail.com>
2024-03-15 20:54:03 +0600

    setup react,tailwind,surge and all with cheek

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='p-4 text-center bg-red-400 text-5xl'>React Assignment Seven</h1>
+    </>
+  )
+}
+
+export default App
